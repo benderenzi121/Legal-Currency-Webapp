ec820fa73cf26e6354462d766309fb041f09befb
mike bajor <michael.bajor@protonmail.com>
2020-11-03 21:03:20 -0500

    reformat and update package-lock.json

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -20,71 +20,61 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', [
-    check('firstName','name is required').not().isEmpty(),
-    check('email', 'please include a valid email').isEmail(),
-    check('password' , 'please enter a password with 6 or more charecters').isLength({min: 6})
-],
-async (req,res) => {
-    const  errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array() });
-    }
-
-    const{firstName,lastName, email, password} = req.body;
-
-    try{
-        
-        let user = await User.findOne({ email })
-
-        if (user){
-            res.status(400).json({errors: [{msg: 'user already exists' }] });
+        check('firstName','name is required').not().isEmpty(),
+        check('email', 'please include a valid email').isEmail(),
+        check('password' , 'please enter a password with 6 or more charecters').isLength({min: 6})
+    ],
+    async (req,res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
         }
 
-        user = new User({
-            firstName,
-            lastName,
-            email, 
-            password
-        });
-        
-       
-        const salt = await bcrypt.genSalt(10);
+        const { firstName, lastName, email, password } = req.body;
 
-        user.password = await bcrypt.hash(password, salt)
-
-    //  saves user to the database
-        await user.save();
-
-    //  This starts the JSON web token part
-        
-    //  creates a payload to be loaded into the token
-        const payload = {
-            user: {
-                id: user.id
+        try{
+            let user = await User.findOne({ email })
+            if (user){
+                res.status(400).json({errors: [{msg: 'user already exists' }] });
             }
+
+            user = new User({ firstName, lastName, email, password });
+        
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt)
+
+        //  saves user to the database
+            await user.save();
+
+        //  This starts the JSON web token part
+            
+        //  creates a payload to be loaded into the token
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+        //  signs the json token using payload, secret key in config.js,
+        //  timer for when the token will expire, callback function to either
+        //  throw error or return the token in a json format
+        
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({ token });
+                }
+            );
+
+
+        } catch(err){
+            console.error(err);
+            res.status(500).send('server error');
         }
-
-    //  signs the json token using payload, secret key in config.js,
-    //  timer for when the token will expire, callback function to either
-    //  throw error or return the token in a json format
-       
-        jwt.sign(
-            payload, 
-            config.get('jwtSecret'),
-            {expiresIn:360000},
-            (err, token) => {
-                if(err) throw err;
-                res.json({ token });
-            }
-            )
-   
-        
-
-    } catch(err){
-        console.error(err);
-        res.status(500).send('server error');
-    }
-    
-});
+    });
 
 module.exports = router;
