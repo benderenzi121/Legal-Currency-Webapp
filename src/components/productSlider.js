1701b56af77e93084521c5d5cfbba98bd6b87134
mike bajor <michael.bajor@protonmail.com>
2020-12-03 16:53:46 -0500

    change class to className in components

--- src/components/productSlider.js
+++ src/components/productSlider.js
@@ -5,56 +5,56 @@ class ProductSlider extends React.Component {
     state = {  }
     render() { 
         return ( 
-            <div class='container-fluid product-slider'>
+            <div className='container-fluid product-slider'>
                 <h2>Featured Collection</h2>
-                <div class='row'> 
-                    <div class='col-md-3 hoverme'>
-                      <div class='product-top'>
-                        <img class='rounded-top' src='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'></img>
+                <div className='row'> 
+                    <div className='col-md-3 hoverme'>
+                      <div className='product-top'>
+                        <img className='rounded-top' src='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'></img>
                         </div>
                         
-                        <div class='product-overlay '>
-                            <button type='button' class='btn btn-secondary' title='View Item'><i class="fas fa-eye"></i></button>
-                            <button type='button' class='btn btn-secondary' title='Add to cart'><i class="fas fa-cart-plus"></i></button>
+                        <div className='product-overlay '>
+                            <button type='button' className='btn btn-secondary' title='View Item'><i className="fas fa-eye"></i></button>
+                            <button type='button' className='btn btn-secondary' title='Add to cart'><i className="fas fa-cart-plus"></i></button>
                         </div>
-                        <div class='product-bottom text-center rounded-bottom'>
+                        <div className='product-bottom text-center rounded-bottom'>
                            <h3>MTG Core 20221</h3> 
                            <h5>$89</h5> 
                         </div>
                     </div>
-                    <div class='col-md-3 hoverme'>
-                      <div class='product-top'></div>  
-                        <img class='rounded-top' src='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'></img>
-                        <div class='product-overlay'>
-                            <button type='button' class='btn btn-secondary' title='Quick Shop'><i class="fas fa-eye"></i></button>
-                            <button type='button' class='btn btn-secondary' title='Quick Shop'><i class="fas fa-cart-plus"></i></button>
+                    <div className='col-md-3 hoverme'>
+                      <div className='product-top'></div>  
+                        <img className='rounded-top' src='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'></img>
+                        <div className='product-overlay'>
+                            <button type='button' className='btn btn-secondary' title='Quick Shop'><i className="fas fa-eye"></i></button>
+                            <button type='button' className='btn btn-secondary' title='Quick Shop'><i className="fas fa-cart-plus"></i></button>
                         </div>
-                        <div class='product-bottom text-center rounded-bottom'>
+                        <div className='product-bottom text-center rounded-bottom'>
                            <h3>MTG Core 20221</h3> 
                            <h5>$89</h5> 
                         </div>
                     </div>
-                    <div class='col-md-3 hoverme'>
-                      <div class='product-top'>  
-                        <img class='rounded-top' src='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'></img>
+                    <div className='col-md-3 hoverme'>
+                      <div className='product-top'>  
+                        <img className='rounded-top' src='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'></img>
                         </div>
-                        <div class='product-overlay'>
-                            <button type='button' class='btn btn-secondary' title='Quick Shop'><i class="fas fa-eye"></i></button>
-                            <button type='button' class='btn btn-secondary' title='Quick Shop'><i class="fas fa-cart-plus"></i></button>
+                        <div className='product-overlay'>
+                            <button type='button' className='btn btn-secondary' title='Quick Shop'><i className="fas fa-eye"></i></button>
+                            <button type='button' className='btn btn-secondary' title='Quick Shop'><i className="fas fa-cart-plus"></i></button>
                         </div>
-                        <div class='product-bottom text-center rounded-bottom'>
+                        <div className='product-bottom text-center rounded-bottom'>
                            <h3>MTG Core 20221</h3> 
                            <h5>$89</h5> 
                         </div>
                     </div>
-                    <div class='col-md-3 hoverme'>
-                      <div class='product-top'></div>  
-                        <img class='rounded-top' src='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'></img>
-                        <div class='product-overlay'>
-                            <button type='button' class='btn btn-secondary' title='Quick Shop'><i class="fas fa-eye"></i></button>
-                            <button type='button' class='btn btn-secondary' title='Quick Shop'><i class="fas fa-cart-plus"></i></button>
+                    <div className='col-md-3 hoverme'>
+                      <div className='product-top'></div>  
+                        <img className='rounded-top' src='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'></img>
+                        <div className='product-overlay'>
+                            <button type='button' className='btn btn-secondary' title='Quick Shop'><i className="fas fa-eye"></i></button>
+                            <button type='button' className='btn btn-secondary' title='Quick Shop'><i className="fas fa-cart-plus"></i></button>
                         </div>
-                        <div class='product-bottom text-center rounded-bottom'>
+                        <div className='product-bottom text-center rounded-bottom'>
                            <h3>MTG Core 20221</h3> 
                            <h5>$89</h5> 
                         </div>
--- src/components/slider.js
+++ src/components/slider.js
@@ -4,36 +4,35 @@ class Slider extends React.Component {
     state = {  }
     render() { 
         return ( 
-            
-            
-               <div id="carouselExampleFade " class="carousel carousel-fade slider" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100 slider-img" src="https://pbs.twimg.com/media/EWNRHGuX0AERfhW.jpg" alt="First slide"/>
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5 class = 'slider-caption-title'>Dragon shield</h5>
-                            <p class = 'slider-caption-desc'>Protect your cards</p>
+            <div id="carouselExampleFade " className="carousel carousel-fade slider" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100 slider-img" src="https://pbs.twimg.com/media/EWNRHGuX0AERfhW.jpg" alt="First slide"/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5 className = 'slider-caption-title'>Dragon shield</h5>
+                            <p className = 'slider-caption-desc'>Protect your cards</p>
                         </div>
                     </div>
-                    <div class="carousel-item">
-                        <img class="d-block slider-img" src="https://www.atlantis-comics.com/wp-content/uploads/2019/02/ddlttkmuaaalbxo_1.jpg" alt="Second slide"/>
+                    <div className="carousel-item">
+                        <img className="d-block slider-img" src="https://www.atlantis-comics.com/wp-content/uploads/2019/02/ddlttkmuaaalbxo_1.jpg" alt="Second slide"/>
                     </div>
-                    <div class="carousel-item">
-                        <img class="d-block slider-img" src="https://pbs.twimg.com/media/EWNRHGuX0AERfhW.jpg" alt="Third slide"/>
+                    <div className="carousel-item">
+                        <img className="d-block slider-img" src="https://pbs.twimg.com/media/EWNRHGuX0AERfhW.jpg" alt="Third slide"/>
                     </div>
                 </div>
-                        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-            
-         );
+
+                <a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+
+                <a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+        );
     }
 }
- 
+
 export default Slider;
