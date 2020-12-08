2955280c4e99cb1d9c024e163d0a1384f16163bf
benderenzi121 <60165030+benderenzi121@users.noreply.github.com>
2020-12-08 01:10:54 -0500

    touched up product slider

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -2,20 +2,21 @@ import React from 'react';
 import '../styles/ProductSlider.css';
 class ProductCard extends React.Component {
     state = {  }
+  
     render() { 
         return ( 
-            <div class='hoverme product'>
-                      <div class='product-top'>
-                        <img class='rounded-top' src='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'></img>
+            <div className='hoverme product'>
+                      <div className='product-top'>
+                        <img className='rounded-top' src={this.props.img}></img>
                         </div>
                         
-                        <div class='product-overlay '>
-                            <button type='button' class='btn btn-secondary' title='View Item'><i class="fas fa-eye"></i></button>
-                            <button type='button' class='btn btn-secondary' title='Add to cart'><i class="fas fa-cart-plus"></i></button>
+                        <div className='product-overlay '>
+                            <button type='button' className='btn btn-secondary' title='View Item'><i className="fas fa-eye"></i></button>
+                            <button type='button' className='btn btn-secondary' title='Add to cart'><i className="fas fa-cart-plus"></i></button>
                         </div>
-                        <div class='product-bottom text-center rounded-bottom'>
-                           <h3>MTG Core 2021</h3> 
-                           <h5>$89</h5> 
+                        <div className='product-bottom text-center rounded-bottom'>
+                           <h3>{this.props.title}</h3> 
+                           <h5>{this.props.price}</h5> 
             </div>
             </div>
          );
--- src/components/productSlider.js
+++ src/components/productSlider.js
@@ -11,20 +11,21 @@ class ProductSlider extends React.Component {
             infinite:true,
             speed:500,
             slidesToShow:4,
-            slidesToScroll:1
+            slidesToScroll:1, 
+            arrows:false
         };
         return ( 
-            <div class='container product-slider'>
+            <div className='container product-slider'>
                 <h2>Featured Collection</h2>
-                  <div class=''> 
+                  <div> 
                         <Slider {...settings}>
                             
-                            <ProductCard/>
-                            <ProductCard/>
-                            <ProductCard/>
-                            <ProductCard/>
-                            <ProductCard/>
-            
+                            <ProductCard title="MTG Core 2020" price ='$89' img='https://i.ebayimg.com/images/g/UScAAOSw0t5e4of2/s-l640.jpg'/>
+                            <ProductCard title="DBZ Vermilion Bloodline" price ='$75' img ='https://i.ebayimg.com/images/g/E18AAOSwsOBfxnd3/s-l1600.jpg'/>
+                            <ProductCard title="Yu-Gi-Oh Phantom Rage" price ='$55' img="https://i.ebayimg.com/images/g/mAcAAOSwkmZfj87N/s-l640.png"/>
+                            <ProductCard title="Freddie Funko Mad Hater" price ='$240' img="https://stockx.imgix.net/products/collectibles/Funko-Pop-Freddy-Funko-as-Mad-Hatter-SDCC-Figure-43.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1603481985"/>
+                            <ProductCard title="MTG Black Lotus" price ='$41999' img="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTUo8Bi8D3QbQCKJ2NMuhB2J-ORBB-rj4ycM5u2BlBhHjUbLOHjIPmqy3dQVP7SBz7U4klPPoDk9zp26yc1nolXWDZqwpmBU3q3uief7L0&usqp=CAc"/>
+                            
                         </Slider>
             </div>
             </div>
