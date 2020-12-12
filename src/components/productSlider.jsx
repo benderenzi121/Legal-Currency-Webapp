30aba7e2ee1b7329c47000058bae53e70669e60f
Ben Derenzi <benderenzi@Bens-MacBook-Air.local>
2020-12-11 22:40:16 -0500

    make productSlider responsive

--- src/components/productSlider.jsx
+++ src/components/productSlider.jsx
@@ -15,6 +15,32 @@ const ProductSlider = (props) => {
         slidesToShow: 4,
         slidesToScroll: 1,
         arrows: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
         // nextArrow: <div className="" />,
         // prevArrow: <Arrow />
     };
