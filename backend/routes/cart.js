4108f616c6541647f66e812c63104db4225e8685
aurelio <aurelioarcabascio@gmail.com>
2021-04-09 13:29:23 -0400

    Reworked add-to-cart route and fixed some bugs

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -116,14 +116,14 @@ router.post('/add-to-cart',[
         //checks field validation
         const errors = validationResult(req);
 
-        if(!errors.isEmpty()){
+        if (!errors.isEmpty()) {
             res.status(400).json({errors:errors.array()});
         };
 
         //Takes token from the header
         const token = req.header('x-auth-token');
-        if (!token){
-            return res.Status(401).json({ msg: 'no token, auth denied'});
+        if (!token) {
+            return res.Status(401).json({ msg: 'no token, auth denied' });
         }
 
         //decode token and find associated user
@@ -132,88 +132,92 @@ router.post('/add-to-cart',[
         
 
         //build cart object
-        try{
+        try {
             //populate from request body
             const {productId, quantity} = req.body;
+						let quantityNumber = Number(quantity)
 
             // fail safe for receiving a negative quantity from the front end
-            if (quantity < 1) {
+            if (quantityNumber < 1) {
                 return res.status(401).json({ error: 'invalid quantity amount' });
             }
 
             //find User using the payload
             let user = await User.findById(userPayload.id);
+
             //get the product from db
             let product = await Product.findById(productId);
             console.log(product);
-            //calculate price of item(s) added to cart
-            let total = ( quantity * product.price);
 
-            //create cart object 
-            //Check to see if cart already exists
-            let iscart = await Cart.findOne({user:user});
-            //there is an existing cart
+            //calculate price of item(s) added to cart
+            let total = ( quantityNumber * product.price);
+
+            /* 	
+							 	|--------------------------|
+								| Creating the cart object |
+								|--------------------------|
+						*/
+
+            // Check to see if cart already exists
+            let isCart = await Cart.findOne({ user });
             
-            if(iscart){
-                let found = false;
-                let i=0;
-                for (i=0;i<iscart.orderItems.length;i++)
-                {
-                    console.log(iscart.orderItems[i]);
-                    if(iscart.orderItems[i].product._id.toString() == product._id.toString()){
-                        found=true;
-                        console.log('found that product!');
-                        iscart.orderItems[i].qty += quantity;
-                        iscart.orderItems[i].total = product.price * iscart.orderItems[i].qty;
-                        try{
-                            iscart.markModified('orderItems');
-                            await iscart.save();
-                            console.log(iscart); 
-                            }
-                        catch(err){
-                            console.error(err);
-                            res.status(500).send('server error');
-                        }
-                        res.status(200).send(iscart.orderItems[i]);
-                        break;
-                    }
-                }
-                if(!found){
-                    await Cart.updateOne(
-                        {user:iscart.user},
-                        {$push:{orderItems:
-                            { 
-                            product:product,
-                            qty:quantity,
-                            total:total 
-                            }}
-                        }
-                        )
-                        res.status(200).send('product pushed to orderItems')
-                }
+						// there is an existing cart
+            if (isCart) {
+							for (let i = 0; i < isCart.orderItems.length; i++) {
+									console.log(isCart.orderItems[i]);
+									if(isCart.orderItems[i].product._id.toString() == product._id.toString()){
+											console.log('found that product!');
+
+											isCart.orderItems[i].qty += quantityNumber;
+											isCart.orderItems[i].total = product.price * isCart.orderItems[i].qty;
+
+											try {
+													isCart.markModified('orderItems');
+													await isCart.save();
+													console.log(isCart); 
+											}
+											catch(err){
+													console.error(err);
+													res.status(500).send('server error');
+													return;
+											}
+											res.status(200).send(isCart.orderItems[i]);
+											return;
+									}
+							}
+
+							await Cart.updateOne(
+									{ user: isCart.user },
+									{ $push: { orderItems:
+											{ 
+											product,
+											qty: quantityNumber,
+											total
+											}}
+									}
+									)
+							res.status(200).send('product pushed to orderItems')
             }
             //there isnt an existing cart so we create one
-            else{
+            else {
                 const cart = new Cart({
                     user,
                     orderItems:
-                   { product:product,
-                    qty:quantity,
-                    total:total
-                   }
+                   	{ product,
+                    	qty: quantityNumber,
+                    	total
+                   	}
                 })
-
                 await cart.save();
                
                 res.status(200).send('cart created and saved');
+								return;
             }
-            
-            
-            
         }
         catch(err){
             console.error(err);
             res.status(500).send('server error');
+						return;
         }
 
 })
